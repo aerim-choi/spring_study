d4a8c56a9e5356cbe4d6133a8c59392cc958846f
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-12-05 11:44:17 +0900

    [스프링 핵심 원리-기본편]섹션6. 컴포넌트스캔(4)
    - 중복 등록과 충돌

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,12 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+    //스프링은 수동빈이 자동빈보다 우선등록권을 가져 오버라이딩 되지만 스프링프레임워크는 중복 빈 등록을 지원하지 않는다.
+    //스프링 부트는 같은 빈을 수동 빈 등록, 자동 빈 등록 시 충돌이 발생한다.=> 스프링 부트 에러를 발생한다.
+
+    //수동빈 등록
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
