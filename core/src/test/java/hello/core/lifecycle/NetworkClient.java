c146ec46a84db531bebcc6cc1094428db47772fc
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-01-01 16:28:50 +0900

    [섹션8]빈 생명주기 콜백(3)
    - 빈 등록 초기화, 소멸 메서드

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,8 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean , DisposableBean {
+
+public class NetworkClient  {
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url); connect();
@@ -24,17 +23,15 @@ public class NetworkClient implements InitializingBean , DisposableBean {
     public void disconnect() {
         System.out.println("close: " + url);
     }
-    //생성후 의존 관계 설정이 되면 실행
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+
+    public void init()  {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
-    //빈이 삭제 될때 호출
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
