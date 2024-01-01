7056b8d473dc877e5a111fb54b67497cb12fec00
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-01-01 16:13:39 +0900

    [섹션8]빈 생명주기 콜백(2)
    - 인터페이스 InitalizingBean,DisposableBean

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,9 @@
 package hello.core.lifecycle;
-public class NetworkClient {
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean , DisposableBean {
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url); connect();
@@ -19,6 +23,19 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+    //생성후 의존 관계 설정이 되면 실행
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+    //빈이 삭제 될때 호출
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
 
