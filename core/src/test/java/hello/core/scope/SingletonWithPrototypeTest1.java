40fdcaf68919a7ca1bb574e89cc02b60ce22c00d
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-01-07 23:34:56 +0900

    [스프링 핵심 원리-기본편]섹션9. 빈 스코프(4)
    - 프로토타입 스코프 : 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,10 +4,14 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -33,30 +37,24 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2= clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
 
 
     }
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;//생성시점에 주입되어버림(그래서 계속 같은 빈을 사용하게됨)
-        @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean=prototypeBean;
-        }
 
-//        @Autowired
-//        ApplicationContext applicationContext; //굉장히 지저분한 코드가 된다.
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic(){
-//            PrototypeBean prototypeBean= applicationContext.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean= prototypeBeanProvider.get();
             prototypeBean.addCount();
-            int count=prototypeBean.getCount();
+            int count = prototypeBean.getCount();
             return count;
         }
-
-
     }
+
     @Scope("prototype")
     static class PrototypeBean{
         private int count = 0;
