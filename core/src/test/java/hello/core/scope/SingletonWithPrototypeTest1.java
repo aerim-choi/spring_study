87cbebdec2527faea09b8271922aff80d346944c
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-01-08 00:10:00 +0900

    [스프링 핵심 원리-기본편]섹션9. 빈 스코프(5~6)
    - 웹 스코프
    - request 스코프 예제 만들기

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -11,7 +11,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
-import javax.inject.Provider;
+import jakarta.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
