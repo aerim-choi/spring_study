e690ea7bcde54a856a4d80a8b81d1b1fa0f3bd1e
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-03-13 23:30:00 +0900

    [실전! 스프링 부트와 JPA 활용1]섹션7. 상품 목록

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,30 +9,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
 
-    @GetMapping("item/new")
-    public String createForm(Model model){
-        model.addAttribute("form",new BookForm());
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
         return "items/createItemForm";
     }
-
-    @PostMapping("items/new")
-    public String create(BookForm form){
+    @PostMapping(value = "/items/new")
+    public String create(BookForm form) {
         Book book = new Book();
+             book.setName(form.getName());
+             book.setPrice(form.getPrice());
+             book.setStockQuantity(form.getStockQuantity());
+             book.setAuthor(form.getAuthor());
+             book.setIsbn(form.getIsbn());
+             itemService.saveItem(book);
+             return "redirect:/items";
+    }
 
-        //setter를 제거하고 createBook하는 것이 더 좋은 설계이다!
-        //실무에서는 setter 안쓴다.
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items",items);
+        return "items/itemList";
 
-        itemService.saveItem(book);
-        return "redirect:/";
     }
 }
