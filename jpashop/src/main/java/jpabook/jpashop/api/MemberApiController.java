76c4f67cf914d468717d406e771a758d566047fa
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-07-02 13:39:40 +0900

    [실전! 스프링부트와 JPA활용2 - API개발과 성능최적화] [실전! 스프링부트와 JPA활용2 - API개발과 성능최적화] 섹션1. API 개발 기본 : 회원 등록 API
    멤버 엔티티명을 수정하면 API가 안동작하면 문제가 생긴다.
    예를들어 Member엔티티의 name이 username으로 바뀌면 API 스펙자체가 바뀌어 버린다.
    => 엔티티를 바꿨더니 API스펙자체가 바껴버리면 문제가 된다.
    => 큰 장애가 발생한다.
    =>API 요청 스펙에 맞춰서 별도의 DTO객체를 파라미터로 해서 받는게 좋다.이게 정석이다!
    실무에서는 로그인 로직만해도 페이스북가입, 간편가입 등등 회원가입 케이스가 있는데 엔티티하나만으로 감당할 수 없음
    =>API스펙 알기 쉽기 때문에 유지보수하기 쉽다.

---
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+           Long id = memberService.join(member);
+           return new CreateMemberResponse(id);
+    }
+
+//    API 요청 스펙에 맞춰서 별도의 DTO를 파라미터로 해서 받는게 좋다.이게 정석이다!
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
